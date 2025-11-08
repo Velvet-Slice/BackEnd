911b85e4901d370eb5ab952c9ad928f09bafd7bc
joaosilvaz <joaovitoraz31@gmail.com>
2025-11-07 22:37:09 -0300

    feat: merge by main

--- src/main/java/com/velvetslice/pi_velvetslice/services/PedidoService.java
+++ src/main/java/com/velvetslice/pi_velvetslice/services/PedidoService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PedidoService {
+public class 'PedidoService {
 
     @Autowired
     private PedidoRepository pedidoRepository;
